7934b5501c1b62b1ca6b00e81dd0d20d0a8b4ded
niamot <myemail>
2026-07-09 00:41:16 +0600

    add packaging script
    - add script to pack the extension with necessary files
    - update README with instruction to package using provided script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Resize Top Bar extension for GNOME 50
 
 Resize top bar height
+
+## Package extension
+
+- Run from extension root dir
+
+    ```shell
+    ./scripts/pack-extension.sh
+    ```
---
+++ scripts/pack-extension.sh
@@ -0,0 +1,6 @@
+zip -r resizetopbar.zip \
+    extension.js \
+    prefs.js \
+    metadata.json \
+    README.md \
+    schemas/org.gnome.shell.extensions.resizetopbar@niam0t.github.io.gschema.xml
